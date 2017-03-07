73eed7e419816e5f1dbc38c7fb656d742f65a622
garo9521 <garo.edgar21@gmail.com>
2017-03-07 16:17:02 -0600

    -

--- Librerias Lectura DICOM/Prueba Garo/Read_File_DICOM_Interfaz.py
+++ Librerias Lectura DICOM/Prueba Garo/Read_File_DICOM_Interfaz.py
@@ -3,6 +3,21 @@ import pylab
 from matplotlib.backend_bases import NavigationToolbar2
 import os, sys
 
+def Umbralizacion(x, y, pixeles):
+	nuevaPixeles = []
+
+	for i in pixeles:
+		vectorAux = []
+		for j in i:
+			vectorAux.append(j - 1000)
+			# if j >= x + 1000 and j <= y + 1000:
+			# 	vectorAux.append(1)
+			# else:
+			# 	vectorAux.append(0)
+		nuevaPixeles.append(vectorAux)
+
+	return nuevaPixeles
+
 home = NavigationToolbar2.home
 back = NavigationToolbar2.back
 forward = NavigationToolbar2.forward
@@ -10,7 +25,7 @@ forward = NavigationToolbar2.forward
 position = 0
 A = []
 path = "/home/alexis/Escritorio/imgDicom"
-path = "C:\Users\lenovo\Documents\GitHub\Trabajo-Terminal\Ejemplo CD DICOM"
+path = "/home/garo/Documents/Trabajo-Terminal/Ejemplos Archivos DICOM/Ejemplo CD DICOM"
 print path
 
 def new_home(self, *args, **kwargs):
@@ -41,7 +56,12 @@ for fil in dirs:
 	aux = path + "/" + fil 
 	A.append(dicom.read_file(aux))			
 
-for i in range (10):
-	pylab.imshow(A[0].pixel_array, cmap=pylab.cm.bone)
+binarizacion = Umbralizacion(0, 100, A[0].pixel_array)
+print binarizacion
+pylab.imshow(binarizacion, cmap = pylab.cm.bone)
+pylab.show()
+pylab.imshow(binarizacion,  cmap='RGB',  interpolation='nearest')
+pylab.show()
+pylab.imshow(A[0].pixel_array, cmap = pylab.cm.bone)
 pylab.show()
 
